3619bef48cae0211cb4719bc56fadd6b2311dfea
mouxangithub <mouxan@163.com>
2025-07-22 11:13:25 +0800

    refactor(selfdrived): 调整消息订阅和忽略逻辑
    - 移除 AlwaysOnDM 条件下的 driverCameraState 忽略判断
    - 优化 SIMULATION 条件下的 ignore 列表添加逻辑
    - 调整 SubMaster 初始化中的消息列表，包括 driverMonitoringState

--- selfdrive/selfdrived/selfdrived.py
+++ selfdrive/selfdrived/selfdrived.py
@@ -85,14 +85,12 @@ class SelfdriveD(CruiseHelper):
     if not self.AlwaysOnDM:
       ignore += ['driverMonitoringState']
     if SIMULATION:
-      ignore += ['managerState']
-      if self.AlwaysOnDM:
-        ignore += ['driverCameraState']
+      ignore += ['driverCameraState', 'managerState']
     if REPLAY:
       # no vipc in replay will make them ignored anyways
       ignore += ['roadCameraState', 'wideRoadCameraState']
     self.sm = messaging.SubMaster(['deviceState', 'pandaStates', 'peripheralState', 'modelV2', 'liveCalibration',
-                                   'carOutput', 'longitudinalPlan', 'livePose', 'liveDelay',
+                                   'carOutput', 'driverMonitoringState', 'longitudinalPlan', 'livePose', 'liveDelay',
                                    'managerState', 'liveParameters', 'radarState', 'liveTorqueParameters',
                                    'controlsState', 'carControl', 'driverAssistance', 'alertDebug', 'userFlag'] + \
                                    self.camera_packets + self.sensor_packets + self.gps_packets + ["longitudinalPlanSP"],
