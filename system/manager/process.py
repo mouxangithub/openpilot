bfc8863f8112031984166b62c5222f2ee4445ff5
mouxangithub <mouxan@163.com>
2025-08-02 21:59:21 +0800

    fix: 移除参数编码指定并清理默认参数列表

--- system/manager/process.py
+++ system/manager/process.py
@@ -276,7 +276,7 @@ class DaemonProcess(ManagerProcess):
     if self.params is None:
       self.params = Params()
 
-    pid = self.params.get(self.param_name, encoding='utf-8')
+    pid = self.params.get(self.param_name)
     if pid is not None:
       try:
         import signal as signal_module
