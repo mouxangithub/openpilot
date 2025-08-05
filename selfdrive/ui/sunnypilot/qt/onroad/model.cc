843ddf7b06f3d12c73407d0b4980a8374ad209a8
mouxangithub <mouxan@163.com>
2025-08-05 09:50:03 +0800

    ui: 重构路径绘制逻辑，将燃料计量器移至sunnypilot实现

--- selfdrive/ui/sunnypilot/qt/onroad/model.cc
+++ selfdrive/ui/sunnypilot/qt/onroad/model.cc
@@ -82,6 +82,8 @@ void ModelRendererSP::drawPath(QPainter &painter, const cereal::ModelDataV2::Rea
 
     painter.setBrush(bg);
     painter.drawPolygon(track_vertices);
+    ModelRenderer::LongFuel(painter, height, width);
+    ModelRenderer::LateralFuel(painter, height, width);
   } else {
     // Normal path rendering
     ModelRenderer::drawPath(painter, model, surface_rect.height(), surface_rect.width());
