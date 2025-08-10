05eed194db025d826e0991265ae7fb6a90d88045
rav4kumar <meetkumardesai@gmail.com>
2025-08-10 13:03:15 -0700

    align

--- selfdrive/ui/qt/onroad/hud.cc
+++ selfdrive/ui/qt/onroad/hud.cc
@@ -157,7 +157,7 @@ void HudRenderer::drawSetSpeed(QPainter &p, const QRect &surface_rect) {
   // Draw outer box + border to contain set speed
   const QSize default_size = {172, 204};
   QSize set_speed_size = is_metric ? QSize(200, 204) : default_size;
-  QRect set_speed_rect(QPoint(60 + (default_size.width() - set_speed_size.width()) / 2, 45), set_speed_size);
+  QRect set_speed_rect(QPoint(40 + (default_size.width() - set_speed_size.width()) / 2, 45), set_speed_size);
 
   // Draw set speed box
   p.setPen(QPen(QColor(255, 255, 255, 75), 6));
