8969c62ddae192dc21267bd897325c5df857553f
mouxangithub <mouxan@163.com>
2025-08-21 22:20:14 +0800

    refactor(system): 移除 dashy_with_video 功能
    - 删除了 dashy_with_video 函数
    - 移除了与 dashy_with_video 相关的进程启动条件
    - 简化了 dashy 函数，使用参数直接判断

--- system/manager/process_config.py
+++ system/manager/process_config.py
@@ -95,9 +95,6 @@ def mapd_ready(started: bool, params: Params, CP: car.CarParams) -> bool:
 def dashy(started: bool, params: Params, CP: car.CarParams) -> bool:
   return int(params.get("dp_dev_dashy") or 0) > 0
 
-def dashy_with_video(started: bool, params: Params, CP: car.CarParams) -> bool:
-  return int(params.get("dp_dev_dashy") or 0) == 2
-
 def or_(*fns):
   return lambda *args: operator.or_(*(fn(*args) for fn in fns))
 
@@ -109,8 +106,7 @@ procs = [
 
   NativeProcess("loggerd", "system/loggerd", ["./loggerd"], logging),
   NativeProcess("encoderd", "system/loggerd", ["./encoderd"], only_onroad),
-  # NativeProcess("stream_encoderd", "system/loggerd", ["./encoderd", "--stream"], notcar),
-  NativeProcess("stream_encoderd", "system/loggerd", ["./encoderd", "--stream"], or_(notcar, and_(dashy_with_video, only_onroad))),
+  NativeProcess("stream_encoderd", "system/loggerd", ["./encoderd", "--stream"], notcar),
   PythonProcess("logmessaged", "system.logmessaged", always_run),
 
   NativeProcess("camerad", "system/camerad", ["./camerad"], driverview, enabled=not WEBCAM),
@@ -154,10 +150,8 @@ procs = [
   PythonProcess("feedbackd", "selfdrive.ui.feedback.feedbackd", only_onroad),
 
   # debug procs
-  # NativeProcess("bridge", "cereal/messaging", ["./bridge"], notcar),
-  # PythonProcess("webrtcd", "system.webrtc.webrtcd", notcar),
-  NativeProcess("bridge", "cereal/messaging", ["./bridge"], or_(notcar, and_(dashy_with_video, only_onroad))),
-  PythonProcess("webrtcd", "system.webrtc.webrtcd", or_(notcar, and_(dashy_with_video, only_onroad))),
+  NativeProcess("bridge", "cereal/messaging", ["./bridge"], notcar),
+  PythonProcess("webrtcd", "system.webrtc.webrtcd", notcar),
   PythonProcess("webjoystick", "tools.bodyteleop.web", notcar),
   PythonProcess("joystick", "tools.joystick.joystick_control", and_(joystick, iscar)),
   PythonProcess("dashy", "dragonpilot.dashy.backend.server", dashy),
