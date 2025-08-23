a95f0b291a5ef4cc4990a63ac592566058d40dc3
mouxangithub <mouxan@163.com>
2025-08-23 10:26:15 +0800

    feat(system): 添加 dashy_with_video 功能并更新相关进程启动条件
    - 新增 dashy_with_video 函数，用于判断是否启用带有视频功能的 dashy
    - 更新 stream_encoderd 和 bridge 进程的启动条件，支持 dashy_with_video
    - 修改 webrtcd 进程的启动条件，支持 dashy_with_video

--- system/manager/process_config.py
+++ system/manager/process_config.py
@@ -95,6 +95,9 @@ def mapd_ready(started: bool, params: Params, CP: car.CarParams) -> bool:
 def dashy(started: bool, params: Params, CP: car.CarParams) -> bool:
   return int(params.get("dp_dev_dashy") or 0) > 0
 
+def dashy_with_video(started: bool, params: Params, CP: car.CarParams) -> bool:
+  return int(params.get("dp_dev_dashy") or 0) == 2
+
 def or_(*fns):
   return lambda *args: operator.or_(*(fn(*args) for fn in fns))
 
@@ -106,7 +109,8 @@ procs = [
 
   NativeProcess("loggerd", "system/loggerd", ["./loggerd"], logging),
   NativeProcess("encoderd", "system/loggerd", ["./encoderd"], only_onroad),
-  NativeProcess("stream_encoderd", "system/loggerd", ["./encoderd", "--stream"], notcar),
+  # NativeProcess("stream_encoderd", "system/loggerd", ["./encoderd", "--stream"], notcar),
+  NativeProcess("stream_encoderd", "system/loggerd", ["./encoderd", "--stream"], or_(notcar, and_(dashy_with_video, only_onroad))),
   PythonProcess("logmessaged", "system.logmessaged", always_run),
 
   NativeProcess("camerad", "system/camerad", ["./camerad"], driverview, enabled=not WEBCAM),
@@ -150,8 +154,10 @@ procs = [
   PythonProcess("feedbackd", "selfdrive.ui.feedback.feedbackd", only_onroad),
 
   # debug procs
-  NativeProcess("bridge", "cereal/messaging", ["./bridge"], notcar),
-  PythonProcess("webrtcd", "system.webrtc.webrtcd", notcar),
+  # NativeProcess("bridge", "cereal/messaging", ["./bridge"], notcar),
+  # PythonProcess("webrtcd", "system.webrtc.webrtcd", notcar),
+  NativeProcess("bridge", "cereal/messaging", ["./bridge"], or_(notcar, and_(dashy_with_video, only_onroad))),
+  PythonProcess("webrtcd", "system.webrtc.webrtcd", or_(notcar, and_(dashy_with_video, only_onroad))),
   PythonProcess("webjoystick", "tools.bodyteleop.web", notcar),
   PythonProcess("joystick", "tools.joystick.joystick_control", and_(joystick, iscar)),
   PythonProcess("dashy", "dragonpilot.dashy.backend.server", dashy),
